bfcaef98a8342288bf271cb4a5fa8cdc2836d507
Mrafisyahbani77 <mrafi.syahbani@gmail.com>
2024-07-16 09:19:52 +0700

    group-hover

--- src/App.jsx
+++ src/App.jsx
@@ -42,11 +42,11 @@ export default function App() {
               {surahList.map((surah) => (
                 <div
                   key={surah.nomor}
-                  className="border rounded-lg p-4 cursor-pointer hover:shadow-md hover:shadow-purple-500 flex items-center justify-between"
+                  className="border rounded-lg group p-4 cursor-pointer hover:shadow-md hover:shadow-purple-500 flex items-center justify-between"
                   onClick={() => handleClickSurah(surah.nomor)}
                 >
                   <div className="flex items-center">
-                    <div className="w-12 h-12 bg-gray-600 transform rotate-45 rounded-lg flex items-center justify-center">
+                    <div className="w-12 h-12 group-hover:bg-purple-500 bg-gray-600 transform rotate-45 rounded-lg flex items-center justify-center">
                       <p className="font-semibold transform -rotate-45">
                         {surah.nomor}
                       </p>
@@ -54,13 +54,13 @@ export default function App() {
                   </div>
                   <div className="">
                     <p className="font-semibold">{surah.nama_latin}</p>
-                    <p className="italic text-xs md:text-sm text-gray-600">
+                    <p className="italic group-hover:text-purple-500 text-xs md:text-sm text-gray-600">
                       {surah.arti}
                     </p>
                   </div>
                   <div className="text-right">
                     <h2 className="text-xl font-semibold">{surah.nama}</h2>
-                    <p>{surah.jumlah_ayat} Ayat</p>
+                    <p className="group-hover:text-purple-500">{surah.jumlah_ayat} Ayat</p>
                   </div>
                 </div>
               ))}
